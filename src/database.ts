3a1c6a599a9228e02e7c80f8b89c8bfebe464f21
Dada abdulrasheed <30962729+iamhabee@users.noreply.github.com>
2023-08-16 17:24:51 +0100

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
 
-export const sequelize = new Sequelize('postgres://postgres:dada@223@localhost:5432/bookshelf');
+export const sequelize = new Sequelize('postgres://libraryuser:WNbkWsYPcffsPZFzAR79G8OiOzR9tnOr@dpg-cjeest8cfp5c73ai9f30-a:5432/librarytest_xo25');
 
   // nOgeu035heRwvauCP0w2
